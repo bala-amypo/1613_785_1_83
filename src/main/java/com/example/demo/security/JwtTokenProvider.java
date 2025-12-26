fba0daa8a6161adee16452a2960d25cf37f5285d
bala-amypo <bala@amypo.in>
2025-12-26 18:37:53 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,18 +1,10 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+
 public class JwtTokenProvider {
-
-    private String secretKey;
-    private long validityInMs;
-
-    public JwtTokenProvider(String secretKey, long validityInMs) {
-        this.secretKey = secretKey;
-        this.validityInMs = validityInMs;
-    }
-
-    public String createToken(String username, String role, long validity) {
-        // implement token creation
-    }
-
     public Claims getClaims(String token) {
-        // implement claim extraction
+        // implement JWT parsing
+        return null;
     }
 }
