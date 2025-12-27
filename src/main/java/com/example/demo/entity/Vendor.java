2ecdbef49bfa8bc4e0b142e7bf20f28702e8dfad
bala-amypo <bala@amypo.in>
2025-12-27 02:19:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vendor.java
+++ src/main/java/com/example/demo/entity/Vendor.java
@@ -1,33 +1,14 @@
-package com.example.demo.model;
-
+@Entity
 public class Vendor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
-    private String contactEmail;
-    private String contactPhone;
-    private Boolean active = true;
 
-    public Vendor() {}
+    private boolean active = true;
 
-    public Vendor(String name, String email, String phone) {
-        this.name = name;
-        this.contactEmail = email;
-        this.contactPhone = phone;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getContactEmail() { return contactEmail; }
-    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
-
-    public String getContactPhone() { return contactPhone; }
-    public void setContactPhone(String contactPhone) { this.contactPhone = contactPhone; }
-
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    // getters & setters
 }
