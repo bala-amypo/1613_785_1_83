22831a80ceac656cac651525076c755478d1eead
bala-amypo <bala@amypo.in>
2025-12-27 05:09:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
     boolean existsByName(String name);
-    Optional<Vendor> findById(Long id);
 }
+
+
